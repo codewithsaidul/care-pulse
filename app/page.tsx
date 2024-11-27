b8c8da6e76e41bb0dec6dc8115236080718633d5
codewithsaidul <codewithsaidul@gmail.com>
2024-11-27 21:17:04 +0600

    Image added on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,44 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
-
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex h-screen max-h-screen">
+
+      {/* ============== TODO: OTP Verification ======================= */}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[495px]">
-          <Image 
-          src="/assets/icons/logo-full.svg"
-          alt="care pulse logo"
-          width={1000}
-          height={1000}
-          className="mb-12 h-10 w-fit"
-           />
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="care pulse logo"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+
+          {/* Home Page */}
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2025 Care Pulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="onboarding image"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%]"
+      />
     </main>
   );
 }
